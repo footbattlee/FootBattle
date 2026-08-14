4f14931844fb95b4c447a403904205cbf7d187c6
footbattlee <footbattlee@gmail.com>
2026-08-14 22:05:48 +0300

    support readable match slug in team balancer

--- app/halisaha-mac/[id]/takimlar/page.tsx
+++ app/halisaha-mac/[id]/takimlar/page.tsx
@@ -1,10 +1,16 @@
 import TeamBalancerClient from "@/components/halisaha/TeamBalancerClient";
+import { extractPublicMatchId } from "@/lib/halisaha/match";
 
 type PageProps = {
   params: Promise<{ id: string }>;
 };
 
 export default async function HalisahaBalancedTeamsPage(props: PageProps) {
-  const { id } = await props.params;
-  return <TeamBalancerClient id={id} />;
+  const { id: routeValue } = await props.params;
+  return (
+    <TeamBalancerClient
+      id={extractPublicMatchId(routeValue)}
+      publicSlug={routeValue}
+    />
+  );
 }
