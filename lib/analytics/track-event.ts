fcb4ba184ad91faec3bd6076f854e73cb5b2201f
footbattlee <footbattlee@gmail.com>
2026-08-17 22:15:22 +0300

    Track surrendered games in analytics

--- lib/analytics/track-event.ts
+++ lib/analytics/track-event.ts
@@ -3,6 +3,7 @@ import { createClient } from "@/lib/supabase/client";
 export type AnalyticsEventName =
   | "game_started"
   | "game_completed"
+  | "game_surrendered"
   | "play_again"
   | "shared";
 
