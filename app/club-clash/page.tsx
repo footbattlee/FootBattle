a480daafd313db848135a8c131e189f50c89624f
footbattlee <footbattlee@gmail.com>
2026-08-20 23:07:30 +0300

    ux: scope Club Clash mobile styles

--- app/club-clash/page.tsx
+++ app/club-clash/page.tsx
@@ -1,1 +1,9 @@
-export { default } from "../api/club-clash/page";
+import ClubClashPage from "../api/club-clash/page";
+
+export default function Page() {
+  return (
+    <div data-game="club-clash">
+      <ClubClashPage />
+    </div>
+  );
+}
