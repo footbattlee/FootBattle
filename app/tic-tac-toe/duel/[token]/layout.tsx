f00da23a354d359c3e788b6897b1085c797d104b
footbattlee <footbattlee@gmail.com>
2026-08-23 11:10:32 +0300

    fix(tic-tac-toe): stabilize mobile player search sheet

---
+++ app/tic-tac-toe/duel/[token]/layout.tsx
@@ -0,0 +1,5 @@
+import "./mobile-fixes.css";
+
+export default function TicTacToeDuelLayout({ children }: { children: React.ReactNode }) {
+  return <div className="ttt-duel-scope">{children}</div>;
+}
