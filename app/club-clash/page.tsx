db21391713006a77980dcf109b4846d3b79cb1cc
footbattlee <footbattlee@gmail.com>
2026-08-25 00:37:15 +0300

    Track Club Clash timeout completion

--- app/club-clash/page.tsx
+++ app/club-clash/page.tsx
@@ -1,8 +1,10 @@
 import ClubClashPage from "../api/club-clash/page";
+import ClubClashCompletionTracker from "@/components/analytics/ClubClashCompletionTracker";
 
 export default function Page() {
   return (
     <div data-game="club-clash">
+      <ClubClashCompletionTracker />
       <ClubClashPage />
     </div>
   );
