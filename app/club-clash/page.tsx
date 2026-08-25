d7f1760184855693d8aa2e65c1a908102ecdbe16
footbattlee <footbattlee@gmail.com>
2026-08-25 08:53:30 +0300

    Show Club Clash result immediately after finish

--- app/club-clash/page.tsx
+++ app/club-clash/page.tsx
@@ -1,10 +1,12 @@
 import ClubClashPage from "../api/club-clash/page";
 import ClubClashCompletionTracker from "@/components/analytics/ClubClashCompletionTracker";
+import ClubClashResultFocus from "@/components/mobile/ClubClashResultFocus";
 
 export default function Page() {
   return (
     <div data-game="club-clash">
       <ClubClashCompletionTracker />
+      <ClubClashResultFocus />
       <ClubClashPage />
     </div>
   );
